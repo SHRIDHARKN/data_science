460b27a79f89d34d60052547229d9215ceba1cec
ShridharDataScientist <74343939+SHRIDHARKN@users.noreply.github.com>
2023-07-14 20:45:45 +0530

    Add files via upload

---
+++ code_ml_from_scratch/kmeans.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+def euclidean_distance(x1, x2):
+    return np.sqrt(np.sum((x1-x2)**2))
+
+class KMeans:
+
+    def __init__(self, K=5, max_iters=100,tolerance=0.001):
+        self.K = K
+        self.max_iters = max_iters
+        self.tolerance = tolerance
+        self.centroids = np.array([])
+        self.clusters = np.array([])
+
+    def _closest_centroid(self, sample, centroids):
+        # distance of the current sample to each centroid
+        distances = [euclidean_distance(sample, point) for point in centroids]
+        closest_idx = np.argmin(distances)
+        return closest_idx
+
+    def _create_clusters(self, centroids):
+        # assign the samples to the closest centroids
+        clusters = [[] for _ in range(self.K)]
+        for idx, sample in enumerate(self.X):
+            centroid_idx = self._closest_centroid(sample, centroids)
+            clusters[centroid_idx].append(idx)
+        return clusters
+
+    def _get_centroids(self, clusters):
+        # assign mean value of clusters to centroids
+        centroids = np.zeros((self.K, self.n_features))
+        for cluster_idx, cluster in enumerate(clusters):
+            cluster_mean = np.mean(self.X[cluster], axis=0)
+            centroids[cluster_idx] = cluster_mean
+        return centroids
+
+    def _is_converged(self, centroids_old, centroids):
+        # distances between old and new centroids, for all centroids
+        distances = [euclidean_distance(centroids_old[i], centroids[i]) for i in range(self.K)]
+        return sum(distances) <= self.tolerance
+    
+    def _get_cluster_labels(self, clusters):
+        # each sample will get the label of the cluster it was assigned to
+        labels = np.empty(self.n_samples)
+        for cluster_idx, cluster in enumerate(clusters):
+           for sample_idx in cluster:
+               labels[sample_idx] = cluster_idx
+
+        return labels
+
+    def fit(self,X):
+        self.X = X
+        self.n_samples, self.n_features = self.X.shape
+        random_sample_idxs = np.random.choice(self.n_samples, self.K, replace=False)
+        self.centroids = [self.X[idx] for idx in random_sample_idxs]
+
+        for _ in range(self.max_iters):
+            # assign samples to closest centroids (create clusters)
+            self.clusters = self._create_clusters(self.centroids)
+            # calculate new centroids from the clusters
+            centroids_old = self.centroids
+            self.centroids = self._get_centroids(self.clusters)
+            if self._is_converged(centroids_old, self.centroids):
+                break
+            
+        
+    def predict(self,X):
+
+        labels = []
+        for x in X:
+            dist_arr = [euclidean_distance(x,centroid) for centroid in self.centroids]
+            labels.append(dist_arr)
+
+        return np.argmin(labels,axis=1)    
