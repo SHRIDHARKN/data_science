4526d413e4af7b1488c8c6c498f62077cbbc33ba
ShridharDataScientist <74343939+SHRIDHARKN@users.noreply.github.com>
2024-01-09 19:19:33 +0530

    Add files via upload

---
+++ code_ml_from_scratch/LogisticRegression.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+
+class LogisticRegression():
+
+    def __init__(self, lr=0.01, n_iters=1000):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weigths = None
+        self.bias = None
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weigths = np.zeros(n_features)
+        self.bias = 0
+
+        for _ in range(self.n_iters):
+            linear_pred = np.dot(X, self.weigths)+self.bias
+            predictions = sigmoid(linear_pred)
+            dw = (1/n_samples)*(np.dot(X.T, (predictions-y)))
+            db = (1/n_samples)*(np.sum(predictions-y))
+            self.weigths = self.weigths-self.lr*dw
+            self.bias = self.bias-self.lr*db
+
+    def predict(self, X):
+        linear_pred = np.dot(X, self.weigths)+self.bias
+        predictions = sigmoid(linear_pred)
+        class_predictions = [0 if x <= 0.5 else 1 for x in predictions]
+        return class_predictions
