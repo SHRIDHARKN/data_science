35bcafae6140b813e63dc8cc3064d241de87e6f3
ShridharDataScientist <74343939+SHRIDHARKN@users.noreply.github.com>
2025-09-07 19:49:17 +0530

    Create params.py

---
+++ utils/params.py
@@ -0,0 +1,5 @@
+CACHE_DIR = "/mnt/g/pretrained_models"
+DATA_CACHE_DIR = "/mnt/g/dev/data"
+EXPERIMENT_CACHE_DIR = "/mnt/g/dev/experiments"
+LANGUAGE_MODELING_TASK = "language_modeling"  # Default task type for the model
+CLASSIFICATION_TASK = "classification"  # Task type for classification models
