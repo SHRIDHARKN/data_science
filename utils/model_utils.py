abaf9a5baf6b577e02ae305ce687b479c5e270e4
ShridharDataScientist <74343939+SHRIDHARKN@users.noreply.github.com>
2025-10-18 22:52:23 +0530

    Update model_utils.py

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,238 +1,132 @@
-import os
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
-
-
-import os
-from PIL import Image
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import pipeline
+from utils.params import LANGUAGE_MODELING_TASK
+from transformers import BitsAndBytesConfig
+from utils.data_utils import log_msg
 import torch
-from torch.utils.data import DataLoader, Dataset
+import os
+import warnings
+warnings.simplefilter("ignore")
 
-import random
-import string
-from termcolor import colored
+bnb_config_4bit = BitsAndBytesConfig(
+    load_in_4bit = True,
+    bnb_4bit_quant_type = 'nf4',
+    bnb_4bit_compute_dtype = torch.bfloat16
+)
 
-import pandas as pd
-import numpy as np
-# create log message
+bnb_config_8bit = BitsAndBytesConfig(
+    load_in_8bit = True
+)
 
-from rich import print
-import logging
-logging.basicConfig(level=logging.INFO)
+def load_img_2_text_hf_pipe(model_name = "Salesforce/blip-image-captioning-large", cache_dir=None):
 
-def show_blue_msg(msg):
-    print(f"[blue]{msg}[/blue]")
-
-def show_reg_msg(msg):
-    print(f"[red]{msg}[/red]")
-
-def show_green_msg(msg):
-    print(f"[green]{msg}[/green]")
-
-def show_orange_msg(msg):
-    print(f"[orange]{msg}[/orange]")
-
-def show_pink_msg(msg):
-    print(f"[pink]{msg}[/pink]")
-
-def get_img_dataloader(data_dir, img_height, img_width, batch_size, data_tag="data", help=False):
-
-    if help:
-        return """
-        train_loader, test_loader, val_loader = get_train_test_val_img_dataloaders(data_dir, img_height, img_width,batch_size)
-        """
-    show_blue_msg(f"data_dir path: {data_dir}")
-    show_blue_msg(f"found subfolds: {os.listdir(data_dir)}")
-    show_reg_msg(f"img_height, img_weight: {img_height}, {img_width}")
-    show_reg_msg(f"batch_size: {batch_size}")
-        
-    data_transforms = {
-        data_tag: transforms.Compose([
-            transforms.Resize((img_height, img_width)),
-            transforms.ToTensor()
-        ]),
-    }
-
-    image_datasets = {data_tag: datasets.ImageFolder(data_dir, data_transforms[data_tag])}
-    show_blue_msg(f"image loader is ready")
-
-    dataloaders = {data_tag: DataLoader(image_datasets[data_tag], batch_size=batch_size, shuffle=True, num_workers=4)}
-
-    img_loader = dataloaders[data_tag]
-    show_blue_msg(f"img_loader is ready")
-
-    return img_loader
-
-
-def get_train_test_val_img_dataloaders(data_dir, img_height, img_width, batch_size, help=False):
-
-    if help:
-        return """
-        train_loader, test_loader, val_loader = get_train_test_val_img_dataloaders(data_dir, img_height, img_width,batch_size)
-        """
-    show_blue_msg(f"data_dir path: {data_dir}")
-    show_blue_msg(f"found subfolds: {os.listdir(data_dir)}")
-    show_reg_msg(f"img_height, img_weight: {img_height}, {img_width}")
-    show_reg_msg(f"batch_size: {batch_size}")
-        
-    data_transforms = {
-        'train': transforms.Compose([
-            transforms.Resize((img_height, img_width)),
-            transforms.ToTensor()
-        ]),
-        'val': transforms.Compose([
-            transforms.Resize((img_height, img_width)),
-            transforms.ToTensor()
-        ]),
-        'test': transforms.Compose([
-            transforms.Resize((img_height, img_width)),
-            transforms.ToTensor()
-        ]),
-    }
-
-    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val', 'test']}
-    show_blue_msg(f"image loader is ready")
-    
-    dataloaders = {x: DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True, num_workers=4) for x in ['train', 'val', 'test']}
-
-    train_loader = dataloaders['train']
-    val_loader = dataloaders['val']
-    test_loader = dataloaders['test']
-    show_blue_msg(f"train_loader is ready")
-    show_blue_msg(f"val_loader is ready")
-    show_blue_msg(f"test_loader is ready")
-    
-    return train_loader, test_loader, val_loader
-
-
-class SingleClassImageDataset(Dataset):
-    def __init__(self, data_dir, img_height, img_width, debug=False):
-        self.data_dir = data_dir
-        self.img_paths = [os.path.join(data_dir, fname) for fname in os.listdir(data_dir)
-                          if fname.lower().endswith(('.png', '.jpg', '.jpeg'))]
-        if debug:
-            self.img_paths = self.img_paths[:10]
-
-        self.transform = transforms.Compose([
-            transforms.Resize((img_height, img_width)),
-            transforms.ToTensor()
-        ])
-
-    def __len__(self):
-        return len(self.img_paths)
-
-    def __getitem__(self, idx):
-        img_path = self.img_paths[idx]
-        img = Image.open(img_path).convert("RGB")
-        img = self.transform(img)
-        return img
-
-
-class ImageTextEmbeddingDataset(Dataset):
-    def __init__(self, csv_file: str, transform=None):
-        
-        self.data_frame = pd.read_csv(csv_file)
-        self.transform = transform
-        # self.data_frame['text_embed'] = self.data_frame['text_embed'].apply(parse_embed)
-
-    def __len__(self):
-        return len(self.data_frame)
-
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        row = self.data_frame.iloc[idx]
-        img_full_path = row['full_paths']
-        image = Image.open(img_full_path).convert('RGB')
-        
-        if self.transform:
-            image = self.transform(image)
-
-        description_text = row['text']
-        text_embedding = self.parse_embed(row['text_embed'])
-        text_embedding = torch.tensor(text_embedding, dtype=torch.float32)
-        return {
-            'image': image,
-            'text_embedding': text_embedding,
-            'description_text': description_text,
-            'img_full_path': img_full_path
-        }
-
-    def parse_embed(self,embed_str):
-        embed_clean = embed_str.strip("[]")
-        return np.fromstring(embed_clean, sep=' ')
+    pipe = pipeline("image-to-text", model=model_name, model_kwargs={"cache_dir": cache_dir})
+    log_msg(msg="Loading image-to-text pipeline")
+    log_msg(f"Model name: {model_name}")
+    log_msg(f"Cache directory: {cache_dir}")
+    log_msg("Pipeline loaded successfully")
+    return pipe
     
 
-def get_img_dataloader(data_dir, img_height, img_width, batch_size, debug=False, help=False):
-    if help:
-        return """img_loader = get_img_dataloader(data_dir, img_height, img_width, batch_size)"""
+def save_df_in_parts(df, records_per_file, data_tag):
 
-    print(f"📁 data_dir path: {data_dir}")
-    print(f"🖼️ found files: {len(os.listdir(data_dir))}")
-    print(f"📐 img_height, img_width: {img_height}, {img_width}")
-    print(f"📦 batch_size: {batch_size}")
-
-    dataset = SingleClassImageDataset(data_dir=data_dir, img_height=img_height, img_width=img_width, debug=debug)
-    img_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=4)
-
-    print(f"✅ img_loader is ready")
-    return img_loader
+    for idx,i in enumerate(range(0,len(df),records_per_file)):
+        df_sample = df.iloc[i:i+records_per_file]
+        save_path = f"/mnt/g/dev/data/emotion/{data_tag}/data_part_{idx+1}.csv"
+        df_sample.to_csv(save_path)
+        print(f">> File data_part_{idx+1}.csv save success.")
 
 
-def generate_id(length=20):
-    """Generate a random alphanumeric ID of specified length."""
-    alpha_num = string.ascii_letters+string.digits
-    _id = "".join(random.choice(alpha_num) for _ in range(length))
-    return _id.lower()
-
-
-# def log_msg(msg, msg_typ="normal", font_color="white", bg_color="green", header="", sep = "="):
+def save_llm_checkpoint(is_peft_model,epoch, checkpoint_dir, avg_epoch_loss,
+                    model, optimizer=None, scheduler=None, 
+                    global_step=None, model_name=None, lr=None, 
+                    weight_decay=None, batch_size=None, num_epochs=None,
+                    gradient_accumulation_steps=None,
+                    model_context_length=None,label_context_length=None,
+                    checkpoint_save_epochs=1):
     
-#     # msg = " \u039B | " +" "*2+ msg +" "*10
+    if (epoch + 1) % checkpoint_save_epochs == 0:
+        os.makedirs(os.path.join(checkpoint_dir,f"checkpoint_epoch_{epoch+1}"), exist_ok=True)
+        checkpoint_path = os.path.join(checkpoint_dir, f"checkpoint_epoch_{epoch+1}/checkpoint.pt")
+        # Save both model state_dict and optimizer/scheduler states for full resume capability
+        if is_peft_model:
+            os.makedirs(os.path.join(checkpoint_dir,f"adapter_epoch_{epoch+1}"), exist_ok=True)
+            print("__________________________________________________________________")
+            print("PEFT model detected. Saving adapter only.")
+            adapter_path = os.path.join(checkpoint_dir, f"adapter_epoch_{epoch+1}")
+            model.save_pretrained(save_directory=adapter_path)
+            print(f"Adapter saved @ {adapter_path}")
+            print("__________________________________________________________________")
+            
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': None if is_peft_model else model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict() if optimizer else None,
+            'scheduler_state_dict': scheduler.state_dict() if scheduler else None,
+            'loss': avg_epoch_loss,
+            'global_step': global_step if global_step else None,
+            'model_name': model_name if model_name else None,
+            'lr': lr if lr else None,
+            'weight_decay': weight_decay if weight_decay else None,
+            'gradient_accumulation_steps': gradient_accumulation_steps if gradient_accumulation_steps else None,
+            'batch_size': batch_size if batch_size else None,
+            'num_epochs': num_epochs if num_epochs else None,
+            'model_context_length': model_context_length if model_context_length else None,
+            'label_context_length': label_context_length if label_context_length else None,
+        }, checkpoint_path)
+        print(f"Checkpoint saved @ {checkpoint_path}")
+        print("__________________________________________________________________")
+
+
+def save_torch_model(states,model_save_path):
+    torch.save(states, model_save_path)
+    print("__________________________________________________________________")
+    print("Model checkpoint saved @ ",model_save_path)
+
+def get_device():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print("__________________________________________________________________")
+    print("Device set to",device)
+    return device
+
+
+def load_pretrained_model(model_name, cache_dir, task, device, bnb_config=None, quant=""):
+
+    """
+    Load a pretrained model and tokenizer from Hugging Face.
     
-#     if msg_typ == "progress":
-#         print(colored(sep * 10 + " INFO " + sep * 10, 'black', 'on_white', attrs=['bold'])) 
-#         print(colored(msg, 'black', 'on_white', attrs=['bold']))   
-#     elif msg_typ == "data":
-#         print(colored(sep * 10 + " DATA/ VARS / CONFIG " + sep * 10, 'black', 'on_white', attrs=['bold'])) 
-#         print(colored(msg, 'white', 'on_blue', attrs=['bold']))
-#     elif msg_typ == "normal":
-#         print(colored(sep * 10 + " PROGRESS/ SAVES " + sep * 10, 'black', 'on_white', attrs=['bold'])) 
-#         print(colored(msg, 'white', 'on_green', attrs=['bold']))
-#     else:
-#         print(colored(sep * 10 + header + sep * 10, 'black', 'on_white', attrs=['bold'])) 
-#         print(colored(msg, font_color, f'on_{bg_color}', attrs=['bold']))
-#     print(colored(sep * 50+"\n", 'black', 'on_white', attrs=['bold'])) 
+    Args:
+        model_name (str): The name of the model to load.
+        cache_dir (str): The directory to cache the model.
+        task (str): The task for which the model is being loaded.
+        bnb_config (BitsAndBytesConfig, optional): Configuration for 4-bit quantization.
 
-def log_msg(msg, font_color="black", bg_color="white"):
-    print(colored(msg, font_color, f'on_{bg_color}', attrs=['bold']))
-    
-class CSVDataset(Dataset):
-    def __init__(self, root_dir):
-        
-        self.root_dir = root_dir
-        self.csv_files = [f for f in os.listdir(root_dir) if f.endswith('.csv')]
-        self.all_data = []
-        self._load_metadata()
-        self.num_files = len(self.csv_files)
-        print(f"Found {self.num_files} CSV files in {self.root_dir}")
+    Returns:
+        tuple: A tuple containing the tokenizer and model.
+    """
+    log_msg("Loading pretrained model and tokenizer")
+    log_msg(f"Model name: {model_name}")
+    log_msg(f"Cache directory: {cache_dir}")
+    log_msg(f"Task: {task}")
 
-    def _load_metadata(self):
-        
-        for file_idx, filename in enumerate(self.csv_files):
-            filepath = os.path.join(self.root_dir, filename)
-            df = pd.read_csv(filepath, header=0)  # Assuming your CSV has a header row
-            for row_idx in range(len(df)):
-                self.all_data.append((file_idx, row_idx))
-                
-    def __len__(self):
-        return len(self.all_data)
+    if bnb_config:
+        quant_4bit = bnb_config._load_in_4bit
+        quant_8bit = bnb_config._load_in_8bit
+        quant = "_4bit_quant" if quant_4bit else "_8bit_quant" if quant_8bit else ""
+        log_msg(msg=f"Quantization config: {quant}")
 
-    def __getitem__(self, idx):
-        file_idx, row_idx = self.all_data[idx]
-        filename = self.csv_files[file_idx]
-        filepath = os.path.join(self.root_dir, filename)
-        df = pd.read_csv(filepath, header=0)
-        sample = df.iloc[row_idx].to_dict()  # Get the row as a dictionary
-        return sample
+    if task==LANGUAGE_MODELING_TASK:
+        tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=cache_dir)
+        model = AutoModelForCausalLM.from_pretrained(model_name, 
+                                                     cache_dir=cache_dir, 
+                                                     quantization_config=bnb_config,
+                                                     device_map="auto" if bnb_config else device)
+
+        tokenizer.pad_token = tokenizer.eos_token
+        model.config.pad_token_id = tokenizer.pad_token_id
+
+        model_tag = model_name.split("/")[-1] # Extract model name for logging
+        model_tag = model_tag+quant if bnb_config else model_tag
+        log_msg(f"Model tag: {model_tag}")
+        log_msg("Model and tokenizer loaded successfully")
+        return tokenizer, model, model_tag
